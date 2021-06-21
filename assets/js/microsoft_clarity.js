b4df0ee01f31850ebbf9814fc9296c4b68856f52
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2021-06-22 05:25:41 +0900

    Remove clarity

--- assets/js/microsoft_clarity.js
+++
@@ -1,6 +0,0 @@
-<!-- Clarity tracking code for https://kyohsuke.github.io/ -->
-(function(c,l,a,r,i,t,y){
-  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
-  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "78fb4hmwo0");
