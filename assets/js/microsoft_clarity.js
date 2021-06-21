f66d8b479e90a6ad8c155592c0a0e3d7f9a55323
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2021-06-22 05:20:40 +0900

    Add microsoft clarity

---
+++ assets/js/microsoft_clarity.js
@@ -0,0 +1,6 @@
+<!-- Clarity tracking code for https://kyohsuke.github.io/ -->
+(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "78fb4hmwo0");
